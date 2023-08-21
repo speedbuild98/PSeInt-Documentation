86185acf708e90b64b6693619af0c7cbb22fc8f2
speedbuild98 <lautaropectm98@gmail.com>
2023-08-21 07:11:33 -0300

    Fixed Layout type error

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
@@ -7,12 +7,18 @@ const roboto = Roboto({
   weight: ["400", "700", "900" , "100", "300", "500"],
 });
 
-const Layout = ({children, id="", justify=""}) => {
-  return (
-    <div id={id} className= {`${roboto.className} px-10 flex min-h-screen flex-col items-center ${justify} bg-base-100 md:px-20`}>
-      {children}
-    </div>
-  )
+interface Props {
+  id?: string;
+  justify?: string;
+  children: React.ReactNode;
 }
 
-export default Layout
+const Layout: React.FC<Props> = ({ children, id, justify }) => {
+  return (
+    <div id={id} className={`${roboto.className} px-10 flex min-h-screen flex-col items-center ${justify} bg-base-100 md:px-20`}>
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
